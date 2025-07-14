dbb4e6c6dc93554475ea6356a39bb9939082ef82
max <islancosta358@gmail.com>
2025-07-13 21:34:32 -0300

    feat: criando adversário humano

--- app/models/personagens/adversarios.py
+++ app/models/personagens/adversarios.py
@@ -1,9 +1,83 @@
 # app/models/inimigo.py
+from dataclasses import dataclass, field
+from typing import Any
 
-class Inimigo:
-    def __init__(self, nome: str, nivel: int, vida: int, dano: int):
-        self.nome = nome
-        self.nivel = nivel
-        self.vida = vida
-        self.dano = dano
+@dataclass
+class AdversarioHumano:
+    nome: str 
+    idade: int
+    peso: int
+    gênero: str
+    altura: float
 
+    nível: int
+    experiência: int
+
+    dano: int
+    velocidade: int
+    defesa: int
+    vida: int
+
+    arma: Any
+    escudo: Any
+
+    classe: Any
+
+    primeira_habilidade_passiva: Any = None
+    segunda_habilidade_passiva: Any = None
+    terceira_habilidade_passiva: Any = None
+
+    primeira_habilidade_ativa: Any = None
+    segunda_habilidade_ativa: Any = None
+
+    descrição: str = field(default = "", init = False)
+
+    def __post_init__(self):
+        self.atualizar_descrição()
+        self.atributos()
+
+    def atualizar_descrição(self) -> None:
+        self.atributos()
+        self.descrição = (f"nome: {self.nome}\n"
+                          f"idade: {self.idade}\n"
+                          f"peso: {self.peso}Kg\n"
+                          f"genero: {self.gênero}\n"
+                          f"altura: {self.altura}m\n"
+                          f"nível: {self.nível}\n"
+                          f"dano: {self.dano}\n"
+                          f"velocidade: {self.velocidade}\n"
+                          f"defesa: {self.defesa}\n"
+                          f"vida: {self.vida}\n"
+                          f"arma: {self.arma}\n"
+                          f"escudo: {self.escudo}\n"
+                          f"classe: {self.classe}\n")
+        
+
+    def atributos(self) -> None:
+        self.dano *= self.nível
+        self.velocidade *= self.nível
+        self.defesa *= self.nível
+        self.vida *= self.nível
+
+    def receber_dano(self, quantidade) -> None:
+        self.vida -= quantidade
+        if self.vida < 0:
+            self.vida = 0
+
+    def atacar(self, alvo: dict) -> None:
+        alvo["vida"] -= self.dano
+
+    def estar_vivo(self):
+        while self.vida > 0:
+            return True
+
+
+class AdversarioDemiHumano:
+    pass
+
+class AdversarioMontro:
+    pass
+
+kanji = AdversarioHumano("kanji", 17, 76, "homem", 1.65, 30, 5, 5, 5, 100, 400, "arco", "", "arqueiro")
+kanji.atualizar_descrição()
+print(kanji.descrição)

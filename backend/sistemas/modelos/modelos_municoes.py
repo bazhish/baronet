196c54bec1ee4beaf4a71ab71c40a8040cd1a0e2
MAX <islancosta358@gmail.com>
2025-09-18 10:20:36 -0300

    ㅤㅤ

--- backend/sistemas/modelos/modelos_municoes.py
+++ backend/sistemas/modelos/modelos_municoes.py
@@ -3,7 +3,7 @@ from random import choice, randint
 from typing import Any, Optional
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
-from backend.app.models.sistema.efeitos_de_combates import (
+from backend.sistemas.efeitos_de_combates import (
     Queimadura, Veneno, Sangramento, Atordoamento, Silencio, Lentidao, Explosivo
 )
 

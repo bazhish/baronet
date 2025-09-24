82c48dbce91276214f028ac92f48e2088268307d
MAX <islancosta358@gmail.com>
2025-09-24 10:07:44 -0300

    ㅤ

--- backend/nucleo/loop_jogo.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from backend.configuracoes import FPS
-from backend.nucleo.gerenciador_cenas import executar_cena
-
-def iniciar_jogo():
-    pygame.init()
-    executar_cena("menu")  # Cena inicial
-    pygame.quit()

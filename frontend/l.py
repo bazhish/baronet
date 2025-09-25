677c81af3bce32453077c96751cf90d6183344ca
joao <00001101178334sp@al.educacao.sp.gov.br>
2025-09-25 18:21:35 -0300

    fix: obj explorar

--- frontend/l.py
+++ frontend/l.py
@@ -1,10 +1,11 @@
 from random import randint
 
-i = [(0, 0), (0, 0), (0, 0)]
-l = [(621, 1017), (683, 1004), (697, 985)]
-y = [(0, 0), (0, 0), (0, 0)]
-t = [(1277, 1380), (1267, 1380), (1255, 1380)]
-o = [285, 304, 314]
+i = [(541, 573), (532, 582), (522, 582)]
+l = [(626, 646), (619, 672), (619, 683)]
+y = [(706, 887), (714, 877), (721, 860)]
+t = [(1053, 1117), (1050, 1131), (1047, 1143)]
+d = [(1254, 1380), (1231, 1380), (1212, 1380)]
+o = [323, 333, 338]
 k = []
 
 for e, r in enumerate(o):
@@ -18,6 +19,9 @@ for e, r in enumerate(o):
         k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
 
     for p in range(t[e][0], t[e][1], 9):
+        k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
+    
+    for p in range(d[e][0], d[e][1], 9):
         k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
     
 print(k)

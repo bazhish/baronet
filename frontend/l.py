39b80bb4e2db48663bbb8c943629770f812b6fcf
joao <00001101178334sp@al.educacao.sp.gov.br>
2025-09-25 13:39:39 -0300

    fix: obj

--- frontend/l.py
+++ frontend/l.py
@@ -2,21 +2,27 @@ from random import randint
 
 i = []
 l = []
-o = [145]
+y = []
+t = []
+o = [148]
 k = []
 
-for p in range(500, 559, 9):
-    i.append(p)
+# for p in range(500, 618, 9):
+#     i.append(p)
 
-for p in range(673, 1380, 9):
-    l.append(p)
+# for p in range(660, 1380, 9):
+#     l.append(p)
 
-l = i + l
+# for p in range(1098, 1165, 9):
+#     y.append(p)
+
+# for p in range(1293, 1380, 9):
+#     t.append(p)
+
+l = i + l + y + t
 
 for y, p in enumerate(o):
     for h in l:
         k.append((h - randint(0, 4), p - randint(4, 7), "arvore_pequena"))
-print(k)
-
-
+print(l)
 

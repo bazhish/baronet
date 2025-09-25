3b6af3de07c562a5d62b561ee02f25f956b8e7fa
joao <00001101178334sp@al.educacao.sp.gov.br>
2025-09-25 16:59:21 -0300

    fix: obj explorar

--- frontend/l.py
+++ frontend/l.py
@@ -1,28 +1,24 @@
 from random import randint
 
-i = []
-l = []
-y = []
-t = []
-o = [148]
+i = [(0, 0), (0, 0), (0, 0)]
+l = [(621, 1017), (683, 1004), (697, 985)]
+y = [(0, 0), (0, 0), (0, 0)]
+t = [(1277, 1380), (1267, 1380), (1255, 1380)]
+o = [285, 304, 314]
 k = []
 
-# for p in range(500, 618, 9):
-#     i.append(p)
+for e, r in enumerate(o):
+    for p in range(i[e][0], i[e][1], 9):
+        k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
 
-# for p in range(660, 1380, 9):
-#     l.append(p)
+    for p in range(l[e][0], l[e][1], 9):
+        k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
 
-# for p in range(1098, 1165, 9):
-#     y.append(p)
+    for p in range(y[e][0], y[e][1], 9):
+        k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
 
-# for p in range(1293, 1380, 9):
-#     t.append(p)
-
-l = i + l + y + t
-
-for y, p in enumerate(o):
-    for h in l:
-        k.append((h - randint(0, 4), p - randint(4, 7), "arvore_pequena"))
-print(l)
+    for p in range(t[e][0], t[e][1], 9):
+        k.append((p - randint(0, 4), r - randint(4, 7), "arvore_pequena"))
+    
+print(k)
 
